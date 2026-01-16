cc9c2e6187d08180a6dcc79fd86f027a08179e98
Xindi Yang <xindi2008hw@gmail.com>
2026-01-16 17:06:46 -0500

    blue close side, 12 balls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_BlueCloseSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_BlueCloseSide.java
@@ -30,17 +30,17 @@ public class Auto_BlueCloseSide extends LinearOpMode {
     int velocity = 1000;
 
     private final Pose startPose = new Pose(56, 8, Math.toRadians(90));
-    private final Pose launchPose1 = new Pose(58, 8.5, Math.toRadians(108));
-    private final Pose launchPose2 = new Pose(60.5, 18.5, Math.toRadians(119));
-    private final Pose launchPose3 = new Pose(62, 21, Math.toRadians(121));
-    private final Pose launchPose4 = new Pose(50, 95.5, Math.toRadians(136));
+    private final Pose launchPose1 = new Pose(58, 8.5, Math.toRadians(109));
+    private final Pose launchPose2 = new Pose(60.5, 18.5, Math.toRadians(118));
+    private final Pose launchPose3 = new Pose(62, 21, Math.toRadians(120));
+    private final Pose launchPose4 = new Pose(65, 95, Math.toRadians(151));
     private final Pose pickup1 = new Pose(56, 27, Math.toRadians(180));
     private final Pose pickup2 = new Pose(56, 53, Math.toRadians(180));
     private final Pose pickup3 = new Pose(56, 79, Math.toRadians(180));
     private final Pose finishPickup1 = new Pose(29, 27, Math.toRadians(180));
     private final Pose finishPickup2 = new Pose(30, 53, Math.toRadians(180));
     private final Pose finishPickup3 = new Pose(33, 79, Math.toRadians(180));
-    private final Pose ending = new Pose(50,30,90);
+    private final Pose ending = new Pose(57,115,146);
 
     private Path scorePreload;
     private PathChain beforePickup1, getPickup1, scorePickup1, beforePickup2, getPickup2, scorePickup2, beforePickup3, getPickup3, scorePickup3, endPath;
@@ -88,8 +88,8 @@ public class Auto_BlueCloseSide extends LinearOpMode {
                 .setLinearHeadingInterpolation(finishPickup3.getHeading(), launchPose4.getHeading())
                 .build();
         endPath = follower.pathBuilder()
-                .addPath(new BezierLine(launchPose3, ending))
-                .setLinearHeadingInterpolation(launchPose3.getHeading(), ending.getHeading())
+                .addPath(new BezierLine(launchPose4, ending))
+                .setLinearHeadingInterpolation(launchPose4.getHeading(), ending.getHeading())
                 .build();
     }
 
@@ -116,7 +116,7 @@ public class Auto_BlueCloseSide extends LinearOpMode {
                 break;
             case 3:
                 if (!follower.isBusy()) {
-                    endIntake();
+                    //endIntake();
                     follower.followPath(scorePickup1);
                     setPathState(4);
                 }
@@ -137,7 +137,7 @@ public class Auto_BlueCloseSide extends LinearOpMode {
                 break;
             case 6:
                 if (!follower.isBusy()) {
-                    endIntake();
+                    //endIntake();
                     follower.followPath(scorePickup2);
                     setPathState(7);
                 }
@@ -159,7 +159,9 @@ public class Auto_BlueCloseSide extends LinearOpMode {
                 break;
             case 9:
                 if (!follower.isBusy()) {
-                    endIntake();
+                    //\
+                    // '/.
+                    // endIntake();
                     follower.followPath(scorePickup3);
                     setPathState(10);
                 }
@@ -281,7 +283,7 @@ public class Auto_BlueCloseSide extends LinearOpMode {
     }
 
     public void startIntake(){
-        intake.setPower(0.5);
+        intake.setPower(0.3);
         ramp.setPower(0.6);
     }
 
